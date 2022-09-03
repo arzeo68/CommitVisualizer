b7e304dabf5997d26c89f9e9b4311a45af8256b3
alexis <alexis.walter@epitech.eu>
2022-09-03 20:35:59 +0200

    master: another test

--- commitVisualizer.sh
+++ commitVisualizer.sh
@@ -1,6 +1,13 @@
 git clone git@github.com:arzeo68/CommitVisualizer.git
 cd CommitVisualizer
-sh ./install.sh
+
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+
 mkdir build
 cd build
 cmake ..
