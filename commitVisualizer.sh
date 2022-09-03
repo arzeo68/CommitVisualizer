992ab6e240acfbb9d9f79d5c0689cef1e94ec702
alexis <alexis.walter@epitech.eu>
2022-09-03 20:38:19 +0200

    master: working version

--- commitVisualizer.sh
+++ commitVisualizer.sh
@@ -1,13 +1,8 @@
-if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
-fi
-
 git clone git@github.com:arzeo68/CommitVisualizer.git
 cd CommitVisualizer
 mkdir build
 cd build
 cmake ..
 make
-cp repositoryStat /usr/local/bin
-echo "----------- END A ------------"
+sudo cp repositoryStat /usr/local/bin
+echo "----------- END ------------"
