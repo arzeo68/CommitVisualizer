3200c0f0055e86270bc4fe965b591b844c1cd9cd
alexis <alexis.walter@epitech.eu>
2022-09-03 20:36:29 +0200

    master: another test

--- commitVisualizer.sh
+++ commitVisualizer.sh
@@ -1,13 +1,10 @@
-git clone git@github.com:arzeo68/CommitVisualizer.git
-cd CommitVisualizer
-
-
 if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
 
-
+git clone git@github.com:arzeo68/CommitVisualizer.git
+cd CommitVisualizer
 mkdir build
 cd build
 cmake ..
